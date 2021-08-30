b53a35a2e3f077983e38c60284c717ee83f22381
sunfl0w <lucaanthonyschwarz@googlemail.com>
2021-08-30 14:07:36 +0200

    Init web application with spring

---
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/SbswebApplication.java
@@ -0,0 +1,13 @@
+package com.lms2ue1.sbsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbswebApplication.class, args);
+	}
+
+}
---
+++ sbsweb/src/test/java/com/lms2ue1/sbsweb/SbswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms2ue1.sbsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
