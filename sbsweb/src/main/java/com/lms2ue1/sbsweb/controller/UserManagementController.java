922dac68d570adabe078425d14e3558bebf6dc1d
sunfl0w <lucaanthonyschwarz@googlemail.com>
2021-09-01 12:56:49 +0200

    Working on additional pages for user management

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/controller/UserManagementController.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/controller/UserManagementController.java
@@ -1,27 +1,47 @@
 package com.lms2ue1.sbsweb.controller;
 
+import com.lms2ue1.sbsweb.model.User;
 import com.lms2ue1.sbsweb.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserManagementController {
     @Autowired
     UserRepository userRepository;
 
-    // How should we handle the home page / ? This will redirect to user management for now
+    // This will redirect home to user management for now
     @GetMapping("/")
     public String showUserManagementRedirect(Model model) {
         model.addAttribute("message", "Testnachricht");
-        return "redirect:userManagement";
+        return "redirect:user_management";
     }
 
-    @GetMapping("/userManagement")
-    public String showUserManagement(Model model) {
+    @GetMapping("/user_management")
+    public String showUserList(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "user_management";
+    }
+
+    @GetMapping("/user_management/new")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "user_new";
+    }
+
+    @GetMapping("/user_management/user/{uid}/delete")
+    public String deleteUserById(@PathVariable Long uid) {
+        userRepository.deleteById(uid);
+        return "redirect:/user_management";
+    }
+
+    @GetMapping("/user_management/user/{uid}/edit")
+    public String editUserById(@PathVariable Long uid, Model model) {
+        model.addAttribute("user", userRepository.findById(uid));
+        return "user_edit";
     }
 }
--- sbsweb/src/main/java/com/lms2ue1/sbsweb/model/User.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/model/User.java
@@ -24,12 +24,20 @@ public class User {
         this.role = role;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
 
     public String getRole() {
         return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {
