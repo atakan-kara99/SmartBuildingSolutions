d82752e7d2a46f1ae3cfd81b2584766652ebd92f
sunfl0w <lucaanthonyschwarz@googlemail.com>
2021-09-08 17:54:42 +0200

    Finished reworking darkmode toggling and it is very satisfying

--- sbsweb/src/main/resources/static/js/darkmode_toggle.js
+++ sbsweb/src/main/resources/static/js/darkmode_toggle.js
@@ -3,9 +3,10 @@
 // Toggles style based on the state of the darkmode toggle switch by sending a POST-Request
 document.getElementById('darkmode-toggle').onchange = function(element) {
     var request = new XMLHttpRequest();
-    request.open("POST", "/set_style", true);
+    request.open("POST", "/set_style", false);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({
         darkmodeEnabled: element.target.checked
     }));
+    location.reload();
 };
