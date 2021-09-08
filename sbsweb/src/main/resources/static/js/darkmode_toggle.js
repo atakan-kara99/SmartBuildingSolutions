db4b51d92004fe93ce750dfbbcbce4c25ecc74d3
sunfl0w <lucaanthonyschwarz@googlemail.com>
2021-09-08 17:56:23 +0200

    Added small comments to code

--- sbsweb/src/main/resources/static/js/darkmode_toggle.js
+++ sbsweb/src/main/resources/static/js/darkmode_toggle.js
@@ -2,11 +2,14 @@
 
 // Toggles style based on the state of the darkmode toggle switch by sending a POST-Request
 document.getElementById('darkmode-toggle').onchange = function(element) {
+    // Send POST request to change style (synchronous)
     var request = new XMLHttpRequest();
     request.open("POST", "/set_style", false);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({
         darkmodeEnabled: element.target.checked
     }));
+
+    // After POST reload page for changes to take effect
     location.reload();
 };
