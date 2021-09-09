45f1730d84d6ba2f3e1f486d25f991af94ec2473
Pumpkin21Spice <83002687+Pumpkin21Spice@users.noreply.github.com>
2021-09-09 15:40:09 +0200

    Merge conflict finally fixed

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/SBSwebApplication.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/SBSwebApplication.java
@@ -1,5 +1,6 @@
 package com.lms2ue1.sbsweb;
 
+import com.lms2ue1.sbsweb.service.DatabaseInitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SBSwebApplication implements CommandLineRunner {
-//    @Autowired
-//    DatabaseInitService databaseInitService;
+    @Autowired
+	DatabaseInitService databaseInitService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SBSwebApplication.class, args);
@@ -16,6 +17,6 @@ public class SBSwebApplication implements CommandLineRunner {
 
     @Override
 	public void run(String... arg0) throws Exception {
-//		databaseInitService.init();
+		databaseInitService.init();
 	}
 }
--- sbsweb/src/main/java/com/lms2ue1/sbsweb/controller/OrganisationManagementController.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/controller/OrganisationManagementController.java
@@ -11,68 +11,71 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class OrganisationManagementController {
 
-////    @Autowired
-////    OrganisationRepository organisationRepository;
-//
-//    /**
-//     * Shows an overview of all organisations.
-//     */
-//    @GetMapping("/organisations")
-//    public String showOrganisationList(Model model) {
-//        model.addAttribute("organisations", organisationRepository.findAll());
-//        return "organisation/organisation_management";
-//    }
-//
-////    @GetMapping("/organisation/{oId}/users")
-////    public String showUsersOfOrganisation(@PathVariable Long oId, Model model) {
-////        model.addAttribute("organisation", organisationRepository.findById(oId).get());
-////        return "organisation/organisation_users";
-////    }
-//
-//    @GetMapping("/organisations/new")
-//    public String showNewOrganisationPage(Model model) {
-//        model.addAttribute("organisation", new Organisation());
-//        return "organisation/organisation_new";
-//    }
-//
-//    @PostMapping("/organisations/save")
-//    public String saveOrganisation(@Valid Organisation organisation, BindingResult bindingResult, Model model) {
-//        // Check if all constraints are met
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("organisation", organisation);
-//            return "organisation/organisation_new";
-//        }
-//        organisationRepository.save(organisation);
-//        return "redirect:/organisations";
-//    }
-//
-//    @GetMapping("/organisation/{oId}/delete")
-//    public String deleteOrganisationByOId(@PathVariable Long oId) {
-//        organisationRepository.deleteById(oId);
-//        return "redirect:/organisations";
-//    }
-//
-//    @GetMapping("/organisation/{oId}/edit")
-//    public String showOrganisationByOId(@PathVariable Long oId, Model model) {
-//        model.addAttribute("organisation", organisationRepository.findById(oId).get());
-//        return "organisation/organisation_edit";
-//    }
-//
-//    @PostMapping("/organisation/{oId}/edit")
-//    public String updateOrganisationById(@PathVariable Long oId,
-//                                         @Valid Organisation organisation, BindingResult bindingResult, Model model) {
-//        // Check if all constraints are met
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("organisation", organisation);
-//            return "organisation/organisation_edit";
-//        }
-//        organisation.setId(oId);
-//        organisationRepository.save(organisation);
-//        return "redirect:/organisations";
-//
-//    }
+    @Autowired
+    OrganisationRepository organisationRepository;
+
+    /**
+     * Shows an overview of all organisations.
+     */
+    @GetMapping("/organisations")
+    public String showOrganisationList(Model model) {
+        model.addAttribute("organisations", organisationRepository.findAll());
+        return "organisation/organisation_management";
+    }
+
+    @GetMapping("/organisation/{oId}/users")
+    public String showUsersOfOrganisation(@PathVariable Long oId, Model model) {
+        model.addAttribute("organisation", organisationRepository.findById(oId).get());
+        model.addAttribute("users", organisationRepository.findById(oId).get().getRoles().stream().map(role -> role.getUsers()).flatMap(List::stream).collect(Collectors.toList()));
+        return "organisation/organisation_users";
+    }
+
+    @GetMapping("/organisations/new")
+    public String showNewOrganisationPage(Model model) {
+        model.addAttribute("organisation", new Organisation());
+        return "organisation/organisation_new";
+    }
+
+    @PostMapping("/organisations/save")
+    public String saveOrganisation(@Valid Organisation organisation, BindingResult bindingResult, Model model) {
+        // Check if all constraints are met
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("organisation", organisation);
+            return "organisation/organisation_new";
+        }
+        organisationRepository.save(organisation);
+        return "redirect:/organisations";
+    }
+
+    @GetMapping("/organisation/{oId}/delete")
+    public String deleteOrganisationByOId(@PathVariable Long oId) {
+        organisationRepository.deleteById(oId);
+        return "redirect:/organisations";
+    }
+
+    @GetMapping("/organisation/{oId}/edit")
+    public String showOrganisationByOId(@PathVariable Long oId, Model model) {
+        model.addAttribute("organisation", organisationRepository.findById(oId).get());
+        return "organisation/organisation_edit";
+    }
+
+    @PostMapping("/organisation/{oId}/edit")
+    public String updateOrganisationById(@PathVariable Long oId,
+                                         @Valid Organisation organisation, BindingResult bindingResult, Model model) {
+        // Check if all constraints are met
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("organisation", organisation);
+            return "organisation/organisation_edit";
+        }
+        organisation.setId(oId);
+        organisationRepository.save(organisation);
+        return "redirect:/organisations";
+
+    }
 }
