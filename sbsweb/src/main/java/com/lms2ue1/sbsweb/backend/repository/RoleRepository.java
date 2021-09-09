652d01291341cb660e6bae4d4719950935fb42cd
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2021-09-09 15:56:51 +0200

    Added method in RoleRepository and started testing

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/repository/RoleRepository.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/repository/RoleRepository.java
@@ -10,4 +10,5 @@ import com.lms2ue1.sbsweb.backend.model.Role;
 
 public interface RoleRepository extends CrudRepository<Role, Long>{
 	List<Role> findByOrganisationOrderByNameAsc(Organisation o);
+	Role findByName(String name);
 }
---
+++ sbsweb/src/test/java/com/lms2ue1/sbsweb/backend/model/AuthorisationCheckTest.java
@@ -0,0 +1,50 @@
+package com.lms2ue1.sbsweb.backend.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.lms2ue1.sbsweb.backend.repository.*;
+
+@SpringBootTest
+class AuthorisationCheckTest {
+	
+	BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+	@MockBean
+	private RoleRepository roleMock;
+	@MockBean
+	private OrganisationRepository orgMock;
+	@MockBean
+	private UserRepository userMock;
+
+	@BeforeEach
+	public void init() {
+		Organisation org0 = new Organisation("SBS");
+    	Organisation org1 = new Organisation("Tiefbau");
+    	
+    	orgMock.save(org0);
+    	orgMock.save(org1);
+    	
+		Role role0 = new Role("SysAdmin", null, null, null, org0);
+		Role role1 = new Role("OrgAdmin", null, null, null, org1);
+
+		roleMock.save(role0);
+		roleMock.save(role1);
+		
+		User user0 = new User("Peter", "Müller", role0, "root", passwordEncoder.encode("admin"));
+		userMock.save(user0);
+	}
+
+	// ------------------ Getter-Tests --------------//
+	@Test
+	void getUserOverRole() {
+		// TODO: Implement me!
+		assertTrue(true);
+	}
+
+}
