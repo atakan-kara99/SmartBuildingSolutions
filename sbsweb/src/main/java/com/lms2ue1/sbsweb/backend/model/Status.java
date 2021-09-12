38feacefc944e9a9f462781576fe0cd6df4bc572
Julius Daum <stu216500@mail.uni-kiel.de>
2021-09-12 12:09:04 +0200

    added protected to methods

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/model/Status.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/model/Status.java
@@ -1,5 +1,6 @@
 package com.lms2ue1.sbsweb.backend.model;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -10,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.mapping.Map;
 
 @Entity
 public class Status {
@@ -32,79 +35,90 @@ public class Status {
 	private List<BillingUnit> billingUnits;
 	@OneToMany(mappedBy = "status")
 	private List<BillingItem> billingItems;
+
+	// TODO: try to implement HashMap<Role, Status>
 	@OneToMany
 	@JoinColumn(name = "next_stati")
-	private List<Status> nextStati;
+	private Map<Role, Status> nextStatus = new HashMap<Role, Status>();
 
 	// ----------------------------------//
 	// ---------- Constructors ----------//
 	// ----------------------------------//
 	public Status() {
 	}
+	/**
+	 * 
+	 * @param name
+	 * @param description
+	 */
+	public Status(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
 
 	// ----------------------------//
 	// ---------- Getter ----------//
 	// ----------------------------//
-	public Long getId() {
+	protected Long getId() {
 		return this.id;
 	}
 
-	public String getName() {
+	protected String getName() {
 		return this.name;
 	}
 
-	public String getDescription() {
+	protected String getDescription() {
 		return this.description;
 	}
 
-	public List<Project> getProject() {
+	protected List<Project> getProject() {
 		return this.projects;
 	}
 
-	public List<Contract> getContract() {
+	protected List<Contract> getContract() {
 		return this.contracts;
 	}
 
-	public List<Organisation> getOrganisation() {
+	protected List<Organisation> getOrganisation() {
 		return this.organisations;
 	}
 
-	public List<BillingUnit> getBillingUnit() {
+	protected List<BillingUnit> getBillingUnit() {
 		return this.billingUnits;
 	}
 
-	public List<BillingItem> getBillingItem() {
+	protected List<BillingItem> getBillingItem() {
 		return this.billingItems;
 	}
 
 	// ----------------------------//
 	// ---------- Setter ----------//
 	// ----------------------------//
-	public void setId(Long id) {
+	protected void setId(Long id) {
 		this.id = id;
 	}
 
-	public void setName(String n) {
+	protected void setName(String n) {
 		this.name = n;
 	}
-	
-	public void setDescription(String desc) {
+
+	protected void setDescription(String desc) {
 		this.description = desc;
 	}
 
-	public void setProject(List<Project> ps) {
+	protected void setProject(List<Project> ps) {
 		this.projects = ps;
 	}
 
-	public void setContract(List<Contract> cs) {
+	protected void setContract(List<Contract> cs) {
 		this.contracts = cs;
 	}
 
-	public void setOrganisation(List<Organisation> os) {
+	protected void setOrganisation(List<Organisation> os) {
 		this.organisations = os;
 	}
 
-	public void setBillingUnit(List<BillingUnit> bus) {
+	protected void setBillingUnit(List<BillingUnit> bus) {
 		this.billingUnits = bus;
 	}
 
