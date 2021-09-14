f5cbdddc85bf01c86fe878dcf5a4621c1bc500b3
Julius Daum <stu216500@mail.uni-kiel.de>
2021-09-14 11:27:20 +0200

    let the shit begin

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/RESTDataRetriever.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/RESTDataRetriever.java
@@ -1,34 +1,30 @@
 package com.lms2ue1.sbsweb.backend.restapi;
 
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 
+import kong.unirest.Unirest;
 import okhttp3.OkHttpClient;
 
 public class RESTDataRetriever {
 
 	// ---- Methods for accessing the data from REST-API ----//
-	public String fetchProjects() {
-		/*OkHttpClient client = new OkHttpClient().newBuilder()
+	public String fetchProjects() throws IOException {
+		OkHttpClient client = new OkHttpClient().newBuilder()
 				  .build();
-		okhttp3.Request request = new Request.Builder()
-				  .url("localhost:3001/api/v1/project/list")
-				  .method("GET", null)
+				okhttp3.Request request = new okhttp3.Request.Builder()
+				  .url("http://localhost:3000/api/v1/project/list")
+				  .method("GET", null).header("Authorization", "Bearer 123")
 				  .build();
-		okhttp3.Response response = client.newCall(request).execute();*/
-	    // TODO: Implement me!
-		return null;
+				okhttp3.Response response = client.newCall(request).execute();
+				System.out.println(response);
+				return response.body().string();
 	}
-
+	
 	public String fetchContracts() {
-		/*OkHttpClient client = new OkHttpClient().newBuilder()
-				  .build();
-				Request request = new Request.Builder()
-				  .url("localhost:3001/api/v1/contracts/:projectId")
-				  .method("GET", null)
-				  *
-				  .build();
-				Response response = client.newCall(request).execute();*/
 	 // TODO: Implement me!
 	    return null;
 	}
