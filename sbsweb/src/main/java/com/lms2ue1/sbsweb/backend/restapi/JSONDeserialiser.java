93b1f75b608626800eef443bf0da31e19501b5d3
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2021-09-14 13:22:13 +0200

    Added todo-comments for later

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/JSONDeserialiser.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/JSONDeserialiser.java
@@ -1,9 +1,13 @@
 package com.lms2ue1.sbsweb.backend.restapi;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lms2ue1.sbsweb.backend.model.*;
 
@@ -13,9 +17,10 @@ public class JSONDeserialiser {
     @Autowired
     RESTDataRetriever restRetriever = new RESTDataRetriever();
     
-    public List<Project> deserialiseProjects() {
-	
-	return mapper.readValue(restRetriever.fetchProjects(), Project[].class);
+    public List<Project> deserialiseProjects() throws JsonMappingException, JsonProcessingException, IOException {
+	// TODO: Alle Assoziationen und adesso-ID!
+	// TODO: JSON-Properties!
+	return Arrays.asList(mapper.readValue(restRetriever.fetchProjects(), Project[].class));
 	
     }
 
