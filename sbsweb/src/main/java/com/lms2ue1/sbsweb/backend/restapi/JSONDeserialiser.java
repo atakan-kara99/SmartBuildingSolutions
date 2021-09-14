2116cb37744c154db368b151d000084d11218993
Julius Daum <stu216500@mail.uni-kiel.de>
2021-09-14 14:30:16 +0200

    updated the json deserializer and the rest data retriever

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/JSONDeserialiser.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/JSONDeserialiser.java
@@ -1,22 +1,51 @@
 package com.lms2ue1.sbsweb.backend.restapi;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Splitter;
 import com.lms2ue1.sbsweb.backend.model.*;
 
 public class JSONDeserialiser {
-    
-    ObjectMapper mapper = new ObjectMapper();
-    @Autowired
-    RESTDataRetriever restRetriever = new RESTDataRetriever();
-    
-    public List<Project> deserialiseProjects() {
+
+	ObjectMapper mapper = new ObjectMapper();
+	@Autowired
+	RESTDataRetriever restRetriever;
+
+	// ---- Converts the rest api data to projects ----//
+	public void deserialiseProjects() throws JsonMappingException, JsonProcessingException, IOException {
+		String json = restRetriever.fetchProjects();
+		
+		List<Project> listProjects = Arrays.asList(mapper.readValue(json, Project.class));
+
+		System.out.println(listProjects);
+
+
+		// return listProjects;
+	}
+	// ---- Converts the rest api data to contracts ----//
+	public void deserializeContracts() throws IOException {
+		String json = restRetriever.fetchContracts(2);
+		
+	}
+	// ---- Converts the rest api data to billung units ----//
+	public void deserializeBillingUnits() throws IOException {
+		String json = restRetriever.fetchBillingUnits(3);
+	}
 	
-	return mapper.readValue(restRetriever.fetchProjects(), Project[].class);
-	
-    }
+	// ---- Converts the rest api data to billing items ----//
+	public void deserializeBillingItems() throws IOException {
+		String json = restRetriever.fetchBillingItems(1);
+	}
+
 
 }
--- sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/RESTDataRetriever.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/restapi/RESTDataRetriever.java
@@ -10,37 +10,41 @@ import okhttp3.Response;
 
 @Component
 public class RESTDataRetriever {
-    
-    /**
-     * URL to connect to the adesso API.
-     */
-    private final String API_URL = "http://localhost:3000/api/v1";
 
-    // ---- Methods for accessing the data from REST-API ----//
-    public String fetchProjects() throws IOException {
-	OkHttpClient client = new OkHttpClient().newBuilder().build();
-	Request request = new Request.Builder()
-		.url(API_URL + "/project/list")
-		.method("GET", null)
-		.header("Authorization", "Bearer 123").build();
-	Response response = client.newCall(request).execute();
-	//System.out.println(response);
-	return response.body().string();
-    }
+	// ---- Constant api url value ----//
+	private final String API_URL = "http://localhost:3000/api/v1";
 
-    public String fetchContracts() {
-	// TODO: Implement me!
-	return null;
-    }
+	// ---- Methods for accessing the data from REST-API ----//
+	public String fetchProjects() throws IOException {
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		Request request = new Request.Builder().url(API_URL + "/project/list").method("GET", null)
+				.header("Authorization", "Bearer 123").build();
+		Response response = client.newCall(request).execute();
+		return response.body().string();
+	}
 
-    public String fetchBillingUnits() {
-	// TODO: implement me!
-	return null;
-    }
+	public String fetchContracts(long projectID) throws IOException {
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		Request request = new Request.Builder().url(API_URL + "/contracts/:" + projectID).method("GET", null)
+				.header("Authorization", "Bearer 123").build();
+		Response response = client.newCall(request).execute();
+		return response.body().string();
+	}
 
-    public String fetchBillingItems() {
-	// TODO: implement me!
-	return null;
-    }
+	public String fetchBillingUnits(long contractID) throws IOException {
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		Request request = new Request.Builder().url(API_URL + "/billingmodel/loadAndParseForContractConfiguration?contractId=" + contractID).method("GET", null)
+				.header("Authorization", "Bearer 123").build();
+		Response response = client.newCall(request).execute();
+		return response.body().string();
+	}
+
+	public String fetchBillingItems(long contractID) throws IOException {
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		Request request = new Request.Builder().url(API_URL + "/contractCompletion/all/:" + contractID).method("GET", null)
+				.header("Authorization", "Bearer 123").build();
+		Response response = client.newCall(request).execute();
+		return response.body().string();
+	}
 
 }
