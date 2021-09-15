bb97ca56b0236c7dfb67870e516f301a06e27f74
Till Kurzenberger <stu215592@mail.uni-kiel.de>
2021-09-15 13:44:03 +0200

    Made setters public again

--- sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/model/Status.java
+++ sbsweb/src/main/java/com/lms2ue1/sbsweb/backend/model/Status.java
@@ -10,105 +10,105 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 
-
 @Entity
 public class Status {
-	// ---- Attributes ----//
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	@NotEmpty
-	private String name;
-	private String description;
+    // ---- Attributes ----//
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotEmpty
+    private String name;
+    private String description;
 
-	// ---- Associations ---- //
-	@OneToMany(mappedBy = "status")
-	private List<Project> projects;
-	@OneToMany(mappedBy = "status")
-	private List<Contract> contracts;
-	@OneToMany(mappedBy = "status")
-	private List<BillingItem> billingItems;
+    // ---- Associations ---- //
+    @OneToMany(mappedBy = "status")
+    private List<Project> projects;
+    @OneToMany(mappedBy = "status")
+    private List<Contract> contracts;
+    @OneToMany(mappedBy = "status")
+    private List<BillingItem> billingItems;
 
-	@OneToMany
-	@JoinColumn(name = "next_stati")
-	private List<Status> nextStati;
+    @OneToMany
+    @JoinColumn(name = "next_stati")
+    private List<Status> nextStati;
 
-	// ----------------------------------//
-	// ---------- Constructors ----------//
-	// ----------------------------------//
-	public Status() {
-	}
-	/**
-	 * 
-	 * @param name
-	 * @param description
-	 */
-	public Status(String name, String description, List<Status> nextStati) {
-		this.name = name;
-		this.description = description;
-		this.nextStati = nextStati;
-	}
+    // ----------------------------------//
+    // ---------- Constructors ----------//
+    // ----------------------------------//
+    public Status() {
+    }
 
-	// ----------------------------//
-	// ---------- Getter ----------//
-	// ----------------------------//
-	public Long getId() {
-		return this.id;
-	}
+    /**
+     * 
+     * @param name
+     * @param description
+     */
+    public Status(String name, String description, List<Status> nextStati) {
+	this.name = name;
+	this.description = description;
+	this.nextStati = nextStati;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    // ----------------------------//
+    // ---------- Getter ----------//
+    // ----------------------------//
+    public Long getId() {
+	return this.id;
+    }
 
-	public String getDescription() {
-		return this.description;
-	}
+    public String getName() {
+	return this.name;
+    }
 
-	public List<Project> getProject() {
-		return this.projects;
-	}
+    public String getDescription() {
+	return this.description;
+    }
 
-	public List<Contract> getContract() {
-		return this.contracts;
-	}
+    public List<Project> getProject() {
+	return this.projects;
+    }
 
-	public List<BillingItem> getBillingItem() {
-		return this.billingItems;
-	}
-	
-	public List<Status> getNextStati() {
-	    return nextStati;
-	}
+    public List<Contract> getContract() {
+	return this.contracts;
+    }
 
-	// ----------------------------//
-	// ---------- Setter ----------//
-	// ----------------------------//
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public List<BillingItem> getBillingItem() {
+	return this.billingItems;
+    }
 
-	public void setName(String n) {
-		this.name = n;
-	}
+    public List<Status> getNextStati() {
+	return nextStati;
+    }
 
-	public void setDescription(String desc) {
-		this.description = desc;
-	}
+    // ----------------------------//
+    // ---------- Setter ----------//
+    // ----------------------------//
+    public void setId(Long id) {
+	this.id = id;
+    }
 
-	public void setProject(List<Project> ps) {
-		this.projects = ps;
-	}
+    public void setName(String n) {
+	this.name = n;
+    }
 
-	public void setContract(List<Contract> cs) {
-		this.contracts = cs;
-	}
-	
-	public void setNextStati(List<Status> nextStati) {
-	    this.nextStati = nextStati;
-	}
+    public void setDescription(String desc) {
+	this.description = desc;
+    }
 
-	public void setBillingItem(List<BillingItem> bis) {
-		this.billingItems = bis;
-	}
+    public void setProject(List<Project> ps) {
+	this.projects = ps;
+    }
+
+    public void setContract(List<Contract> cs) {
+	this.contracts = cs;
+    }
+
+    public void setNextStati(List<Status> nextStati) {
+	this.nextStati = nextStati;
+    }
+
+    public void setBillingItem(List<BillingItem> bis) {
+	this.billingItems = bis;
+    }
 
 }
